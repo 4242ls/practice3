f6c89df41105d1231114afda2f717385c2086b42
Lia <liasargsyan93@gmail.com>
2025-05-15 16:31:49 -0400

    2nd commit

--- src/main/java/TM1.java
+++ src/main/java/TM1.java
@@ -3,4 +3,5 @@ public class TM1 {
 
 public static void main(String[] args) {
     System.out.println("hekjiejfhjf");
+    System.out.println("love");
 }
---
+++ src/main/java/bekind1.java
@@ -0,0 +1,5 @@
+public class bekind1 {
+    public static void main(String[] args) {
+        System.out.println(" sirir");
+    }
+}
